b44ae2697b619213c624fd0fa6786600e37cd21b
ManhLe <manhle93@gmail.com>
2020-07-30 17:17:40 +0700

    fuck

--- resources/views/hoadon.blade.php
+++ resources/views/hoadon.blade.php
@@ -7,14 +7,23 @@
 
 <body onload="window.print(); myFunction()">
     <div id="app">
-        <h3 style="text-align: center">HÓA ĐƠN BÁN HÀNG</h3>
-        <div style="text-align: center; font-size: 18px">Ngày {{$ngay}} tháng {{$thang}} năm {{$nam}}</div>
+        <div style="display:flex; flex-direction: row;  align-items: center; justify-content: center">
+            <div> <img src="http://ruongbacthang.skymapglobal.vn/static/img/logorbt.5dcc5da9.jpg" style="width: 180px"></div>
+            <div style="margin-left: 20px">
+                <p style="font-size: 16px; font-weight: bold;">RUỘNG BẬC THANG</p>
+                <p>Địa chỉ: 54 Ngọc Hân Công Chúa - Ninh Xá - Tp Bắc Ninh</p>
+                <p>SĐT: 0988.861.479 - 0862968081</p>
+            </div>
+        </div>
         <br>
-        <div class="line"><strong>1. Đơn hàng </strong>{{$data->ten}}</div>
-        <div class="line"><strong>2. Mã đơn hàng: </strong>{{$data->ma}}</div>
-        <div class="line"><strong>3. Người mua hàng: </strong>{{$data->user_id ? $data->user->name : 'Khách lẻ'}}</div>
-        <div class="line"><strong>4. Phương thức thanh toán </strong>{{$data->thanh_toan}}</div>
-        <div class="line"><strong>5. Ghi chú </strong>{{ $data->ghi_chu}}</div>
+        <h3 style="text-align: center; font-size: 14px">HÓA ĐƠN BÁN HÀNG</h3>
+        <div style="text-align: center; font-size: 13px">Ngày {{$ngay}} tháng {{$thang}} năm {{$nam}}</div>
+        <br>
+        <div class="line"><strong>Đơn hàng </strong>{{$data->ten}}</div>
+        <div class="line"><strong>Mã đơn hàng: </strong>{{$data->ma}}</div>
+        <div class="line"><strong>Người mua hàng: </strong>{{$data->user_id ? $data->user->name : 'Khách lẻ'}}</div>
+        <div class="line"><strong>Phương thức thanh toán: </strong>{{$data->thanh_toan}}</div>
+        <div class="line"><strong>Ghi chú: </strong>{{ $data->ghi_chu}}</div>
         <br>
         <table style="border: 2px solid black; border-collapse: collapse; width: 100%">
             <thead>
@@ -42,6 +51,11 @@
         <div class="line"><strong>Giảm giá: </strong><span id="giamgia">{{ $data->giam_gia}}</span></div>
         <div class="line"><strong>Đã thanh toán: </strong><span id="dathanhtoan">{{ $data->da_thanh_toan}}</span></div>
         <div class="line"><strong>Còn phải thanh toán: </strong><span id="conphaithanhtoan">{{ $data->con_phai_thanh_toan}}</span></div>
+        <div style="display:flex; flex-direction: column;  align-items: center; justify-content: center">
+                <div>-------------------------------------------------</div>
+                <div style="font-size: 14px; font-weight: bold; ">CẢM ƠN QUÝ KHÁCH VÀ HẸN GẶP LẠI!</div>
+                <div>website:ruongbacthang.com.vn</div>
+        </div>
     </div>
 </body>
 <style>
